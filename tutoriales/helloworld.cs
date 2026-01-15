cd6412a00462c36f64189b7f19831af03d5cb509
NazRub <rubilnazareno@gmail.com>
2026-01-15 08:28:27 -0300

    .

---
+++ tutoriales/helloworld.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("hello world") ;
+string aFriend = "bill" ;
+Console.WriteLine($"Hello {aFriend}");
+aFriend = "Maira";
+Console.WriteLine($"Hello {aFriend}");
---
+++ tutoriales/opcadenas.cs
@@ -0,0 +1,5 @@
+string firstFriend = "Maria" ; 
+string secondFriend = "Sage" ;
+//Console.WriteLine($"My friend are {firstFriend} and {secondFriend} ") ;
+Console.WriteLine($"The name {firstFriend.ToUpper()} has {firstFriend.Length} letters") ;
+Console.WriteLine($"The name {secondFriend.ToLower()} has {secondFriend.Length} letters") ;
