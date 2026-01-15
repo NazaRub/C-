6e9b6aee3c5573acde5d8dcb31703b1ee42b92ed
NazRub <rubilnazareno@gmail.com>
2026-01-15 11:38:51 -0300

    /

---
+++ 02_numeros/04_double.cs
@@ -0,0 +1,7 @@
+// como el INT tiene limites minimos y maximos, veremos otros TYPES que puedan usarse en caso de sobrepasar esos limites
+ // OPERACIONES DOUBLE
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+console.w
