cf050af6a9ec2d17a636b62856dbae4528bcf225
NazRub <rubilnazareno@gmail.com>
2026-01-15 09:29:52 -0300

    tutorial

---
+++ 01_tutoriales/04_replace.cs
@@ -0,0 +1,8 @@
+/* METODO REPLACE, toma dos parametross, busca una subcadena y la reemplaza por texto
+string sayHello = "Hello world!" ;
+Console.WriteLine(sayHello) ;
+sayHello = sayHello.Replace("Hello", "Greetings") ;
+Console.WriteLine(sayHello) ; 
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+*/
---
+++ 01_tutoriales/05_contains.cs
@@ -0,0 +1,8 @@
+string songLyrics = "You say goodbye, and i say hello" ;
+Console.WriteLine(songLyrics.StartsWith("You")) ; 
+Console.WriteLine(songLyrics.StartsWith("goodbye"));
+
+Console.WriteLine(songLyrics.EndsWith("hello"));
+Console.WriteLine(songLyrics.EndsWith("goodbye"));
+///El metodo CONTAINS devuelve un valor BOOLEANO
+/// Stars y Ends devuelve un booleano, busca en la subcadena de una cadena 
