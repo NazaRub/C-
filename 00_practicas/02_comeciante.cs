fe331b90a2163fd2ede93a01f93a6b95b080a88f
NazRub <rubilnazareno@gmail.com>
2026-01-16 16:05:26 -0300

    .

---
+++ 00_practicas/02_comeciante.cs
@@ -0,0 +1,9 @@
+int agua = 3 ; 
+int dinero = 100 ; 
+int distancia = 5 ; 
+int distanciaRecorrido = 0 ;
+
+ while ((distanciaRecorrido < 5) && (agua > 0))
+{
+    Console.WriteLine("Que queres hacer?\n") ;
+}
