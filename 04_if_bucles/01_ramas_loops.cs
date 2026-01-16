f683c312d55dc4c9c36dce8eec88cb58248331b9
NazRub <rubilnazareno@gmail.com>
2026-01-16 13:59:53 -0300

    Update 01_ramas_loops.cs

--- 04_if_bucles/01_ramas_loops.cs
+++ 04_if_bucles/01_ramas_loops.cs
@@ -1,6 +1,10 @@
 int a = 5 ;
 int b = 3 ;
-if ( a+ b > 10)
-   Console.WriteLine("La respuesta es mas grande que 10") ;
-else 
+int c = 4 ;
+
+ if (( a + b = c > 10) && (a == b)) ;{
+   Console.WriteLine("La respuesta es mas grande que 10") ; } 
+  else
+   {
    Console.WriteLine("la respuesta es menor que 10") ;
+   }
