39a881424c0a8f73f1a7610ea913a3052dd2a95d
NazRub <rubilnazareno@gmail.com>
2026-01-17 11:36:44 -0300

    .

---
+++ 04_if_bucles/03_do_while.cs
@@ -0,0 +1,7 @@
+int counter = 0 ;
+do 
+{
+ Console.WriteLine($"La cuenta va por: {counter}") ;
+ counter ++ ;
+}
+while (counter <= 10) ;
---
+++ 04_if_bucles/04_for.cs
@@ -0,0 +1,5 @@
+for (int counter = 0; counter <=10; counter++) 
+///  inicializador /// Coindicion // iterador.
+{
+    Console.WriteLine($"La cuenta va por: {counter}") ;
+}
