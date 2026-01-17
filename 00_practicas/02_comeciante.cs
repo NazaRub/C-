58dc9d74a427eda7cd741eae766a1414e22e1807
NazRub <rubilnazareno@gmail.com>
2026-01-16 22:54:07 -0300

    Update 02_comeciante.cs

--- 00_practicas/02_comeciante.cs
+++ 00_practicas/02_comeciante.cs
@@ -2,8 +2,16 @@ int agua = 3 ;
 int dinero = 100 ; 
 int distancia = 5 ; 
 int distanciaRecorrido = 0 ;
+int opcion = 0 ;
 
  while ((distanciaRecorrido < 5) && (agua > 0))
 {
-    Console.WriteLine("Que queres hacer?\n") ;
+    Console.WriteLine("Que queres hacer?\n1-Avanzar\n2-comprar agua") ;
+    opcion = int.Parse(Console.ReadLine()) ;
+    if (opcion == 1)
+    {
+        agua -= 1 ;
+        distancia -= 1 ;
+        distanciaRecorrido +=  1 ;
+    }
 }
