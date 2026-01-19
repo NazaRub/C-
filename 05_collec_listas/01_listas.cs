57fa3cba23835ded2ea4f316efec7ff3f1de7b3d
NazRub <rubilnazareno@gmail.com>
2026-01-19 10:02:07 -0300

    .

--- 05_collec_listas/01_listas.cs
+++ 05_collec_listas/01_listas.cs
@@ -1,5 +1,9 @@
-List<string> names = ["<name>", "Ana", "Felipe"];
+List <string> names = ["<name>", "Ana", "Felipe"];
+
 foreach (var name in names)
+
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+Console.WriteLine("Ya no quedan mas nombres!") ;
+// cada vez que el codigo vuelve arriba para agarrar el siguiente nombre, decimos que hizo una iteracion//
