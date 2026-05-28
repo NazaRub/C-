886569f27627e63ceb33cdc29aa956ea77f052f8
Nazareno <narubil.it@gmail.com>
2026-05-28 18:30:22 -0300

    .

--- Junio26/Varibales/01_char_int.cs
+++ Junio26/Varibales/01_char_int.cs
@@ -1,2 +1,8 @@
 Console.WriteLine('b') ; // al ser un solo caractr con '' corre sin problemas
 Console.WriteLine (123) ; //los enteros no necesitan comillas a menos que querramos que sean string
+Console.WriteLine (0.25F) ;
+// para convertir el numero en FLOAT se le agrega la F al final.
+Console.WriteLine(2.625);
+// para un DOUBLE no es necesario agregar nada ya que el compilador tiene como valor predeterminado DOUBLE cuando no hay un sufijo mas.
+Console.WriteLine(12.39816m) ;
+// Para crear u DECIMAL literal se le agrega le sufijo m al final del numero.
