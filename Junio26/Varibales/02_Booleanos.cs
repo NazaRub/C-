c35681f445b36e7c75958ec002cdf8a43109138d
Nazareno <narubil.it@gmail.com>
2026-05-28 22:54:08 -0300

    .

---
+++ Junio26/Varibales/02_Booleanos.cs
@@ -0,0 +1,11 @@
+Console.WriteLine(true) ;
+Console.WriteLine(false) ;
+/*
+La conclusión principal es que hay muchos tipos de datos, pero se centrará en solo unos cuantos por ahora:
+
+string para palabras, frases o cualquier dato alfanumérico para la presentación, no cálculo
+char para un único carácter alfanumérico
+int para un número entero
+decimal para un número con un componente fraccionario
+bool para un true/false valor
+*/ 
