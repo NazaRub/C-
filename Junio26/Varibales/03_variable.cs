1b90929afa110a4ac3376a516198dd524771bb5e
Nazareno <narubil.it@gmail.com>
2026-05-29 21:01:10 -0300

    varibles

--- Junio26/Varibales/03_variable.cs
+++ Junio26/Varibales/03_variable.cs
@@ -2,3 +2,8 @@ using System.Data.SqlTypes;
 
 string firstName ;
 firstName = "Nazareno" ;
+ /*
+ ° Las variables son valores temporales que se almacenan en la memoria del equipo.
+ ° Para poder usar una var debo declararla.
+ ° Se declara diciendo el tipo de dato que quiero que almacene, el nombre y contenido.
+ */
---
+++ Junio26/Varibales/04_ejercicioVar.cs
@@ -0,0 +1,10 @@
+string firstName = "naz" ;
+Console.WriteLine (firstName) ;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
